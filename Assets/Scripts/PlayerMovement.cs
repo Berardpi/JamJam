f457a73d6e253554ff89dc122c4949e28746647f
jibjib <bardin.jeanbaptiste78@gmail.com>
2022-05-21 11:45:28 +0200

    Animate player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public Rigidbody2D body;
+    public SpriteRenderer sprite;
     public float moveSpeed;
+    public Animator animator;
 
     private Vector2 inputVect = Vector2.zero;
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(inputVect.x * moveSpeed, rb.velocity.y);
+        body.velocity = new Vector2(inputVect.x * moveSpeed, body.velocity.y);
+        UpdateAnimations();
     }
 
     void OnMove(InputValue value)
     {
         inputVect = value.Get<Vector2>();
+    }
+
+    void UpdateAnimations()
+    {
+        animator.SetFloat("speed", Mathf.Abs(body.velocity.x));
+        if (body.velocity.x > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (body.velocity.x < 0)
+        {
+            sprite.flipX = true;
+        }
     }
 }
